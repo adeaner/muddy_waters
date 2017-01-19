ffebd9530b3a2ee86dd4d33c740e8ab0ea33e091
Ashley Deaner <ashley.deaner@digitalglobe.com>
2017-01-18 18:23:35 -0700

    check point - stashing pixel values

--- src/main/java/io/geobigdata/muddy/SampleApp.java
+++ src/main/java/io/geobigdata/muddy/SampleApp.java
@@ -2,49 +2,24 @@ package io.geobigdata.muddy;
 
 
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
+import java.awt.image.DataBufferUShort;
+import java.io.*;
 import java.net.URL;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.net.URLEncoder;
-import java.util.List;
-import java.util.ArrayList;
-
 import javax.imageio.ImageIO;
-import javax.net.ssl.HostnameVerifier;
 import javax.xml.parsers.ParserConfigurationException;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.Polygon;
-import com.vividsolutions.jts.io.WKTWriter;
 import de.topobyte.osm4j.core.access.OsmIterator;
 import de.topobyte.osm4j.core.model.iface.EntityContainer;
 import de.topobyte.osm4j.core.model.iface.EntityType;
 import de.topobyte.osm4j.core.model.iface.OsmNode;
 import de.topobyte.osm4j.core.model.iface.OsmWay;
 import de.topobyte.osm4j.xml.dynsax.OsmXmlIterator;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.util.EntityUtils;
 import org.xml.sax.SAXException;
-
 import com.digitalglobe.gbdx.tools.auth.GBDXAuthManager;
 
 import java.io.IOException;
-import java.util.Collections;
 
 import com.digitalglobe.gbdx.tools.catalog.CatalogManager;
 import com.digitalglobe.gbdx.tools.catalog.model.Record;
@@ -58,8 +33,11 @@ public class SampleApp {
     public static void main(String[] args) throws ParserConfigurationException,
             SAXException, IOException {
 
-        Double bbox[] = {41.87205347180925, 12.441844940185547, 41.9155632172071, 12.509737014770506};
-        String wkt = "POLYGON ((-122.41189956665039 37.59415685597818, -122.41189956665039 37.64460175855099, -122.34529495239259 37.64460175855099, -122.34529495239259 37.59415685597818, -122.41189956665039 37.59415685597818))";
+        // upper left lat/lon, lower right lat/lon
+        Double bbox[] = {39.84670129520201, -104.99307632446288, 39.801810432481645, -104.92518424987793};
+        //BoundingBox upperLeftLatitude=39.92843137829837, upperLeftLongitude=-105.05199104547503, lowerRightLatitude=39.89999167197872, lowerRightLongitude=-104.9971452355385
+        // counter clockwise lon/ lat
+        String wkt = String.format("POLYGON((%2$f %1$f, %4$f %1$f, %4$f %3$f, %2$f %3$f, %2$f %1$f))", bbox[0], bbox[1], bbox[2], bbox[3]);
 
 
         Map<Long, OsmNode> nodesById = getFeatures(bbox);
@@ -67,34 +45,49 @@ public class SampleApp {
         String idaho_id_multi = getIdahoId(wkt);
 
 
+        // Get idaho chip
+        String baseUrl = "http://idaho.geobigdata.io/v1";
+        GBDXAuthManager gbdxAuthManager = new GBDXAuthManager();
+
+        String pathUrl = String.format("/chip/centroid/idaho-images/%s?", idaho_id_multi);
+
         List pixelvalues = new ArrayList();
         // Iterate nodes to get pixel values
         for (OsmNode value : nodesById.values()) {
-            System.out.println("value = " + value);
 
             Double lat = value.getLatitude();
             Double lon = value.getLongitude();
 
-            // Get idaho chip
-            String baseUrl = "http://idaho.geobigdata.io/v1";
-            GBDXAuthManager gbdxAuthManager = new GBDXAuthManager();
-
-            String pathUrl = String.format("/chip/centroid/idaho-images/%s?", idaho_id_multi);
 
             String idaho_query = String.format("lat=%s&long=%s" +
-                    "&width=10&height=10&resolution=0.3" +
+                    "&width=1&height=1&resolution=0.3&bands=7,6,5,4,3,2,1,0&format=tif" +
                     "&token=%s", lat, lon, gbdxAuthManager.getAccessToken());
 
             BufferedImage img = null;
 
             try {
+//                Iterator<ImageReader> readers = ImageIO.getImageReadersBySuffix("TIF");
+//                while(readers.hasNext()){
+//                    ImageReader reader = readers.next();
+//                    System.out.println(reader.getClass().getName());
+//                }
+
                 URL idaho_url = new URL(baseUrl + pathUrl + idaho_query);
-                img = ImageIO.read(idaho_url);
+                InputStream input = idaho_url.openStream();
+
+                img = ImageIO.read(input);
+
+
             } catch (IOException e) {
+                System.out.println(e);
             }
 
-
-            pixelvalues.add(img);
+            try {
+                final short[] pixels = ((DataBufferUShort) img.getRaster().getDataBuffer()).getData();
+                pixelvalues.add(pixels);
+            } catch (NullPointerException e) {
+                System.out.println(e);
+            }
 
         }
 
@@ -107,6 +100,7 @@ public class SampleApp {
      * @throws IOException
      */
     private static Map<Long, OsmNode> getFeatures(Double bbox[]) throws IOException {
+        //bbox lower left, upper right
         String feature_query = String.format(
                 "/*\n" +
                         "Waterways\n" +
@@ -116,7 +110,8 @@ public class SampleApp {
                         "  (%f,%f,%f,%f);\n" +
                         "/*add way to node*/\n" +
                         "(._;>;);\n" +
-                        "out;", bbox[0], bbox[1], bbox[2], bbox[4]);
+                        "out;", bbox[2], bbox[1], bbox[0], bbox[3]);
+        //(39.80959097923673,-105.1779556274414,39.98895805956577,-104.90638732910156)
 
         String feature_url = "http://overpass-api.de/api/interpreter?data=" + URLEncoder.encode(feature_query, "UTF-8");
 
@@ -164,7 +159,7 @@ public class SampleApp {
         SearchRequest searchRequest = new SearchRequest();
 
         searchRequest.withSearchAreaWkt(wkt)
-                .withFilters(Collections.singletonList("cloudCover < 20, sensorPlatformName = 'WV03'"))
+                .withFilters(Arrays.asList("sensorPlatformName = 'WV03'", "cloudCover < 20", "colorInterpretation = 'WORLDVIEW_8_BAND'"))
                 .withTypes(Collections.singletonList("IDAHOImage"));
 
         SearchResponse response = catalogManager.search(searchRequest);
@@ -179,6 +174,6 @@ public class SampleApp {
 
         // sort results, most recent
 
-        return "";
+        return ""; //null
     }
 }
